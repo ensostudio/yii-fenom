67ae4f499f2e8d062439a9c0d14ca1e8c6da92b3
Anton Fedonyuk <info@ensostudio.ru>
2022-06-12 12:57:07 +0300

    Add test case and configuration

---
+++ tests/FenomViewRendererTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ensostudio\yii2fenom;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * FenomViewRenderer tests
+ */
+class FenomViewRendererTest extends TestCase
+{
+    public function testRender()
+    {
+        // @todo add tests
+    }
+}
